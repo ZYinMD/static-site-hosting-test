47205c293d2004ecc5d17bcb34fa42154590322f
Z Yin <Z.Yin.MD@gmail.com>
2022-04-09 17:11:24 -0400

    modify vercel function

--- public/api/helloworld.js
+++ public/api/helloworld.js
@@ -1,4 +1,15 @@
 export default function handler(request, response) {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  response.status(200).send(`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Connection Test</title>
+	</head>
+	<body>
+	  <h1>hello world</h1>
+	</body>
+	</html>
+   `);
 }
